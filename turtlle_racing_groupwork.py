ebeefccc950432ec7d3e9082d840c12b440e8c5a
Osae-ux <oscarforman@seattleacademy.org>
2025-05-16 16:24:30 +0000

    Turtle Race group work

--- turtlle_racing_groupwork.py
+++ turtlle_racing_groupwork.py
@@ -17,18 +17,16 @@ def create_sprite(image_filename, x=0, y=0):
 	return sprite
 
 
-# Section 2 - Variables
-# TODO - add starting values for all the variables
-x1 = -10
-y1 = -10
-x2 = -10
-y2 = -20
-x3 = random.randint(100,120)
-y3 = 0
-x4 = -10
-y4 = -30
+# Section 2 - adding the Variables
+x1 = -250
+y1 = 250
+x2 = -250
+y2 = 175
+x3 = -250
+y3 = 100
+x4 = -250
+y4 = 25
 # Section 3 - Setup
-# TODO - use your own background, and set your four turtles to images of your choice
 set_background("castle")
 t1 = create_sprite("tik",x1,y1)
 t2 = create_sprite("fred",x2,y2)
@@ -36,10 +34,10 @@ t3 = create_sprite("food",x3,y3)
 t4 = create_sprite("chuck",x4,y4)
 
 
-# # Section 4 - Racing
-# # TODO - set how much each variable changes by and increase the number of repeats to at least 30
-# # TODO - explain here which sprites are faster or slower
-for i in range(3):
+# Racing part
+# x2 is fastest because the lowest speed is still faster than x3's fastest speed
+time.sleep(3)
+for i in range(5):
 	x1 += 20
 	x2 += random.randint(100,120)
 	x3 += 90
@@ -48,17 +46,18 @@ for i in range(3):
 	t2.goto(x2, y2)
 	t3.goto(x3, y3)
 	t4.goto(x4, y4)
-	time.sleep(0.1)
+	
 
 
-# # Section 5 - Winner
-# # TODO - complete the elif for player 2 winning
-# # TODO - write another elif for player 3 and player 4
-# if x1 >= x2 and x1 >= x3 and x1 >= x4:
-# 	print("player 1 wins!")
-# elif
-# 	print("player 2 wins!")
-
+# Section 5 - Winner
+if x1 >= x2 and x1 >= x3 and x1 >= x4:
+ 	print("tik wins!")
+elif x2 >= x1 and x2 >= x3 and x2 >= x4:
+	print("fred wins!")
+elif x3 >= x1 and x3 >= x2 and x3 >= x4:
+	print("food wins!")
+elif x4 >= x1 and x4 > x2 and x4 >= x1:
+	print("chuck wins!")
 
 
 
